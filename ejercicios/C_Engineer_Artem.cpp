50b5a82794514fd36984d46b49742d58a732e672
Mauricio Carazas Segovia <mauriciocarazas9812@gmail.com>
2022-09-28 10:15:59 -0500

    Add files via upload

--- ejercicios/C_Engineer_Artem.cpp
+++ ejercicios/C_Engineer_Artem.cpp
@@ -2,6 +2,24 @@
 using namespace std;
 
 int main(){
-
-  return 0;
+	int T;  
+    cin>>T;
+	while(T--){     // number of cases
+		int n, m;  
+        cin>>n>>m; // n - number of rows, m - number of columns
+		for(int i = 1; i <= n; i++){ // (i,j), either bi,j=ai,j or bi,j=ai,j+1.
+			for(int j = 1; j <= m; j++){
+				int x;  cin>>x;
+				if((i+j)%2==0 && x%2==0){
+                    x++;
+                }
+				if((i+j)%2==1 && x%2==1){
+                    x++;
+                }   
+				cout<<x<<" ";
+			}
+			cout<<"\n";
+		}
+	}
+	return 0;
 }
