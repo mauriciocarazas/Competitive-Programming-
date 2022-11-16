8c4fcc4e5c6882dc040ef55eb48502ee8a859d51
Mauricio Carazas Segovia <mauriciocarazas9812@gmail.com>
2022-11-16 10:24:03 -0500

    Update floyd_warshall.cpp

--- Graph/floyd_warshall.cpp
+++ Graph/floyd_warshall.cpp
@@ -1,7 +1,6 @@
 //Floyd Warshall Algorithm
 #include <stdio.h>
 
-// Number of vertices in the graph
 #define V 4
 
 #define INF 99999
@@ -17,18 +16,14 @@ void floydWarshall(int graph[][V]){
 			dist[i][j] = graph[i][j];
 
 	for (k = 0; k < V; k++) {
-		// Pick all vertices as source one by one
 		for (i = 0; i < V; i++) {
-			// Pick all vertices as destination for the above picked source
 			for (j = 0; j < V; j++) {
-				// If vertex k is on the shortest path from i to j, then update the value of dist[i][j]
 				if (dist[i][k] + dist[k][j] < dist[i][j])
 					dist[i][j] = dist[i][k] + dist[k][j];
 			}
 		}
 	}
 
-	// Print the shortest distance matrix
 	printSolution(dist);
 }
 
@@ -51,15 +46,17 @@ void printSolution(int dist[][V])
 
 int main()
 {
-	/* 
-			10
-	(0)------->(3)
-		|		 /|\
-	5 |		 |
-		|		 | 1
-	\|/		 |
-	(1)------->(2)
-			3		 */
+	/*
+	   10
+      (0)——->(3)
+        |    /|\
+     5 |      |  1
+        |     |  
+       \|/    
+      (1)——->(2)
+           3 
+	
+	*/
 	int graph[V][V] = { { 0, 5, INF, 10 },
 						{ INF, 0, 3, INF },
 						{ INF, INF, 0, 1 },
