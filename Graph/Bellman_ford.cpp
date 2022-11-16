5ce0bfa00b59de93343067a55d471ea9ee3a5ed8
Mauricio Carazas Segovia <mauriciocarazas9812@gmail.com>
2022-11-16 10:45:29 -0500

    Update Bellman_ford.cpp

--- Graph/Bellman_ford.cpp
+++ Graph/Bellman_ford.cpp
@@ -1,5 +1,3 @@
-// A C++ program for Bellman-Ford's single source
-// shortest path algorithm.
 #include <iostream>
 #include <queue>
 #include <vector>
@@ -9,22 +7,16 @@
 #include <stdio.h>
 using namespace std;
 
-// a structure to represent a weighted edge in graph
 struct Edge {
 	int src, dest, weight;
 };
 
-// a structure to represent a connected, directed and
-// weighted graph
 struct Graph {
-	// V-> Number of vertices, E-> Number of edges
 	int V, E;
 
-	// graph is represented as an array of edges.
 	struct Edge* edge;
 };
 
-// Creates a graph with V vertices and E edges
 struct Graph* createGraph(int V, int E)
 {
 	struct Graph* graph = new Graph;
@@ -34,7 +26,6 @@ struct Graph* createGraph(int V, int E)
 	return graph;
 }
 
-// A utility function used to print the solution
 void printArr(int dist[], int n)
 {
 	printf("Vertex Distance from Source\n");
@@ -42,24 +33,17 @@ void printArr(int dist[], int n)
 		printf("%d \t\t %d\n", i, dist[i]);
 }
 
-// The main function that finds shortest distances from src
-// to all other vertices using Bellman-Ford algorithm. The
-// function also detects negative weight cycle
+
 void BellmanFord(struct Graph* graph, int src)
 {
 	int V = graph->V;
 	int E = graph->E;
 	int dist[V];
 
-	// Step 1: Initialize distances from src to all other
-	// vertices as INFINITE
 	for (int i = 0; i < V; i++)
 		dist[i] = INT_MAX;
 	dist[src] = 0;
 
-	// Step 2: Relax all edges |V| - 1 times. A simple
-	// shortest path from src to any other vertex can have
-	// at-most |V| - 1 edges
 	for (int i = 1; i <= V - 1; i++) {
 		for (int j = 0; j < E; j++) {
 			int u = graph->edge[j].src;
@@ -71,10 +55,6 @@ void BellmanFord(struct Graph* graph, int src)
 		}
 	}
 
-	// Step 3: check for negative-weight cycles. The above
-	// step guarantees shortest distances if graph doesn't
-	// contain negative weight cycle. If we get a shorter
-	// path, then there is a cycle.
 	for (int i = 0; i < E; i++) {
 		int u = graph->edge[i].src;
 		int v = graph->edge[i].dest;
@@ -82,8 +62,7 @@ void BellmanFord(struct Graph* graph, int src)
 		if (dist[u] != INT_MAX
 			&& dist[u] + weight < dist[v]) {
 			printf("Graph contains negative weight cycle");
-			return; // If negative cycle is detected, simply
-					// return
+			return; 
 		}
 	}
 
@@ -92,12 +71,12 @@ void BellmanFord(struct Graph* graph, int src)
 	return;
 }
 
-// Driver's code
+
 int main()
 {
-	/* Let us create the graph given in above example */
-	int V = 5; // Number of vertices in graph
-	int E = 8; // Number of edges in graph
+
+	int V = 5; 
+	int E = 8;
 	struct Graph* graph = createGraph(V, E);
 
 	// add edge 0-1 (or A-B in above figure)
@@ -140,8 +119,7 @@ int main()
 	graph->edge[7].dest = 3;
 	graph->edge[7].weight = -3;
 	
-	// Function call
-	BellmanFord(graph, 0);
+	bellmanFord(graph, 0);
 
 	return 0;
 }
