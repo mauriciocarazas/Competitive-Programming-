fba0e91abf2b193d0b513f2d8187791b8c7abbab
Mauricio Carazas Segovia <mauriciocarazas9812@gmail.com>
2022-11-16 10:21:11 -0500

    Update Dijkstra_Heap.cpp

--- Graph/Dijkstra_Heap.cpp
+++ Graph/Dijkstra_Heap.cpp
@@ -1,8 +1,5 @@
-/*Time Complexity: O(E * logV), Where E is the number of edges and V is the number of vertices.
-Auxiliary Space: O(V)*/
+//Complexity: O(E * logV), Where E is the number of edges and V is the number of vertices.
 
-// C++ Program to find Dijkstra's shortest path using
-// priority_queue in STL
 #include <iostream>
 #include <queue>
 #include <vector>
@@ -13,25 +10,17 @@ Auxiliary Space: O(V)*/
 using namespace std;
 #define INF 0x3f3f3f3f
 
-// iPair ==> Integer Pair
 typedef pair<int, int> iPair;
 
-// This class represents a directed graph using
-// adjacency list representation
 class Graph {
-	int V; // No. of vertices
-
-	// In a weighted graph, we need to store vertex
-	// and weight pair for every edge
+	int V; 
 	list<pair<int, int> >* adj;
 
 public:
-	Graph(int V); // Constructor
+	Graph(int V);
 
-	// function to add an edge to graph
 	void addEdge(int u, int v, int w);
 
-	// prints shortest path from s
 	void shortestPath(int s);
 };
 
@@ -48,50 +37,27 @@ void Graph::addEdge(int u, int v, int w)
 	adj[v].push_back(make_pair(u, w));
 }
 
-// Prints shortest paths from src to all other vertices
 void Graph::shortestPath(int src)
 {
-	// Create a priority queue to store vertices that
-	// are being preprocessed. This is weird syntax in C++.
-	// Refer below link for details of this syntax
-	// https://www.geeksforgeeks.org/implement-min-heap-using-stl/
 	priority_queue<iPair, vector<iPair>, greater<iPair> >
 		pq;
 
-	// Create a vector for distances and initialize all
-	// distances as infinite (INF)
 	vector<int> dist(V, INF);
 
-	// Insert source itself in priority queue and initialize
-	// its distance as 0.
 	pq.push(make_pair(0, src));
 	dist[src] = 0;
 
-	/* Looping till priority queue becomes empty (or all
-	distances are not finalized) */
 	while (!pq.empty()) {
-		// The first vertex in pair is the minimum distance
-		// vertex, extract it from priority queue.
-		// vertex label is stored in second of pair (it
-		// has to be done this way to keep the vertices
-		// sorted distance (distance must be first item
-		// in pair)
+		
 		int u = pq.top().second;
 		pq.pop();
 
-		// 'i' is used to get all adjacent vertices of a
-		// vertex
 		list<pair<int, int> >::iterator i;
 		for (i = adj[u].begin(); i != adj[u].end(); ++i) {
-			// Get vertex label and weight of current
-			// adjacent of u.
 			int v = (*i).first;
 			int weight = (*i).second;
-
-			// If there is shorted path to v through u.
 			if (dist[v] > dist[u] + weight) {
-				// Updating distance of v
-				dist[v] = dist[u] + weight;
+								dist[v] = dist[u] + weight;
 				pq.push(make_pair(dist[v], v));
 			}
 		}
@@ -103,14 +69,11 @@ void Graph::shortestPath(int src)
 		printf("%d \t\t %d\n", i, dist[i]);
 }
 
-// Driver's code
 int main()
 {
-	// create the graph given in above figure
 	int V = 9;
 	Graph g(V);
-
-	// making above shown graph
+	
 	g.addEdge(0, 1, 4);
 	g.addEdge(0, 7, 8);
 	g.addEdge(1, 2, 8);
@@ -126,7 +89,6 @@ int main()
 	g.addEdge(6, 8, 6);
 	g.addEdge(7, 8, 7);
 
-	// Function call
 	g.shortestPath(0);
 
 	return 0;
