9a4ab1c75c28eec1fbdbac7da90b63dac078dfe8
Mauricio Carazas Segovia <mauriciocarazas9812@gmail.com>
2022-11-23 10:47:44 -0500

    Create G. Fall Down.cpp

---
+++ ejercicios/G. Fall Down.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+const char EMPTY = '.';
+const char STONE = '*';
+const char OBSTACLE = 'o';
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, m;
+    cin >> n >> m;
+    char g[n][m];
+    for (int r = 0; r < n; r++) {
+      for (int c = 0; c < m; c++) {
+        cin >> g[r][c];
+      }
+    }
+    for (int r = n - 1; r > -1; r--) {
+      for (int c = 0; c < m; c++) {
+        if (g[r][c] == STONE) {
+          int nr = r;
+          while (nr < n - 1 and g[nr + 1][c] == EMPTY) {
+            nr++;
+          }
+          g[r][c] = EMPTY;
+          g[nr][c] = STONE;
+        }
+      }
+    }
+
+    //cout <<endl<<endl<< "solution:"<<endl<<endl;
+    for (int r = 0; r < n; r++) {
+      for (int c = 0; c < m; c++) {
+        cout << g[r][c];
+      }
+      cout << "\n";
+    }
+  }
+  return 0;
+}
